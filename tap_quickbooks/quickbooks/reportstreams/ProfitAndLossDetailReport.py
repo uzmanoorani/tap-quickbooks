4970d64e778902cc8830b6594dcd3cfd435c8346
uzmanoorani <uzma.bibi@eurustechnologies.com>
2026-07-14 14:55:05 +0500

    updated

--- tap_quickbooks/quickbooks/reportstreams/ProfitAndLossDetailReport.py
+++ tap_quickbooks/quickbooks/reportstreams/ProfitAndLossDetailReport.py
@@ -143,7 +143,7 @@ class ProfitAndLossDetailReport(BaseReportStream):
             min_time = datetime.datetime.min.time()
             today = datetime.date.today()
             today = datetime.datetime.combine(today, min_time)
-            max_requests = 3
+            max_requests = 4
             fetch_cols = cols.copy()
 
             # params for concurrent requests
